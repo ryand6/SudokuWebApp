6b84d3e95a7ec793808a51126aa93289f73c1a64
R D <ryandowney64@yahoo.com>
2024-08-09 23:19:17 +0100

    Fixed dependency injection for BoardStateController

--- src/main/java/com/github/ryand6/sudokuweb/config/PuzzleGenerationConfig.java
+++ src/main/java/com/github/ryand6/sudokuweb/config/PuzzleGenerationConfig.java
@@ -1,16 +1,11 @@
 package com.github.ryand6.sudokuweb.config;
 
-import com.github.ryand6.sudokuweb.services.PuzzleGenerator;
-import com.github.ryand6.sudokuweb.services.impl.PuzzleGeneratorFromFile;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class PuzzleGenerationConfig {
-
-    @Bean
-    public PuzzleGenerator puzzleGenerator() {
-        return new PuzzleGeneratorFromFile();
-    }
-
-}
+//@Configuration
+//public class PuzzleGenerationConfig {
+//
+//    @Bean
+//    public PuzzleGenerator puzzleGenerator() {
+//        return new PuzzleGeneratorFromFile();
+//    }
+//
+//}
--- src/main/java/com/github/ryand6/sudokuweb/controllers/BoardStateController.java
+++ src/main/java/com/github/ryand6/sudokuweb/controllers/BoardStateController.java
@@ -7,13 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-;
 
 @RestController
 public class BoardStateController {
 
+    private final PuzzleGenerator puzzleGenerator;
+
+    public BoardStateController(PuzzleGenerator puzzleGenerator) {
+        this.puzzleGenerator = puzzleGenerator;
+    }
+
     @GetMapping("/generate-board")
-    public List<String> generateSudoku(@RequestParam String difficulty, PuzzleGenerator puzzleGenerator) {
+    public List<String> generateSudoku(@RequestParam String difficulty) {
         // Call static method to generate puzzle as a nested int array based on the
         // user's difficulty setting
         return puzzleGenerator.generatePuzzle(difficulty);
