6e82be0328ba8c2ca039bcd31308b1024f12ebf2
R D <ryandowney64@yahoo.com>
2025-08-15 19:09:39 +0100

    Added CORS configuration to allow requests from frontend to backend

--- backend/src/main/java/com/github/ryand6/sudokuweb/config/SecurityConfig.java
+++ backend/src/main/java/com/github/ryand6/sudokuweb/config/SecurityConfig.java
@@ -9,6 +9,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -23,14 +28,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                // Enable CORS
                 .cors(Customizer.withDefaults())
+                // Publish csrf token in a cookie that React SPA can read
                 .csrf((csrf) -> csrf
                         // Send the csrf token as a cookie that is readable by React so that it can attach this to HTTP request headers
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+
                 // Configure when authorization is required
                 .authorizeHttpRequests(auth -> auth
                         // Require that any URL requires authentication except for homepage, css files, and user set-up
-                        .requestMatchers("/", "/error/**", "/webjars/**").permitAll()
+                        .requestMatchers("/", "/error/**").permitAll()
                         .anyRequest().authenticated()
                 )
 
@@ -52,6 +60,22 @@ public class SecurityConfig {
                 //);
 
         return http.build();
+    }
+
+    // Used by default in http.cors() config
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        // React origin
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        // Required for JSESSIONID
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type", "X-XSRF-TOKEN"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // Applies configuration to all endpoints in the backend
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 
