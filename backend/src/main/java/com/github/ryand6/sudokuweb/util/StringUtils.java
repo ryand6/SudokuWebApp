e9ce2d0bc7243438254c33f4114913a7790a8561
R D <ryandowney64@yahoo.com>
2025-09-14 17:43:31 +0100

    Added StringUtils methods to assist with text validation and applied these to custom validation annotation implemnentation

--- backend/src/main/java/com/github/ryand6/sudokuweb/util/StringUtils.java
+++ backend/src/main/java/com/github/ryand6/sudokuweb/util/StringUtils.java
@@ -1,13 +1,58 @@
 package com.github.ryand6.sudokuweb.util;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public final class StringUtils {
 
-    private StringUtils() {
+    // Immutable static map used for normalisation
+    private static final Map<String, List<String>> CHAR_SUBSTITUTIONS;
 
-    };
+    static {
+        // Standard hashmap is mutable
+        Map<String, List<String>> tempMap = new HashMap<>();
+        tempMap.put("a", List.of("@", "4"));
+        tempMap.put("e", List.of("3"));
+        tempMap.put("i", List.of("1", "!"));
+        tempMap.put("o", List.of("0"));
+        tempMap.put("b", List.of("8", "6"));
+        tempMap.put("c", List.of("<"));
+        tempMap.put("g", List.of("9"));
+        tempMap.put("s", List.of("$", "5"));
+        tempMap.put("t", List.of("7", "+"));
 
-    public static String normaliseString() {
+        // Make immutable copy of map and store as constant
+        CHAR_SUBSTITUTIONS = Map.copyOf(tempMap);
+    }
 
+    // private constructor prevents class instantiation
+    private StringUtils() {};
+
+    // Convert string to lowercase, strip leading and trailing whitespace, substitute leetspeak chars
+    public static String normaliseString(String s) {
+        s = s.toLowerCase().strip();
+        return s.codePoints().mapToObj(c -> new String(Character.toChars(c))).map(StringUtils::substituteChar).collect(Collectors.joining());
+    }
+
+    // Loop through substitutions map to see if char is found in any of the value sets, and if so replace with the key
+    private static String substituteChar(String c) {
+        for (String key: CHAR_SUBSTITUTIONS.keySet()) {
+            if (CHAR_SUBSTITUTIONS.get(key).contains(c)) {
+                return key;
+            }
+        }
+        return c;
+    }
+
+    public static String removeNonAlphabeticChars(String s) {
+        return s.replaceAll("[^A-Za-z]", "");
+    }
+
+    // Check that all characters in string within ASCII range by checking their unicode values (ASCII = 0 to 127)
+    public static boolean isAsciiString(String s) {
+        return s.chars().allMatch(c -> c < 128);
     }
 
 }
--- backend/src/main/java/com/github/ryand6/sudokuweb/validation/ProfanityValidator.java
+++ backend/src/main/java/com/github/ryand6/sudokuweb/validation/ProfanityValidator.java
@@ -1,9 +1,12 @@
 package com.github.ryand6.sudokuweb.validation;
 
 import com.github.ryand6.sudokuweb.config.ProfanityDictionaryLoader;
+import com.github.ryand6.sudokuweb.util.StringUtils;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import org.springframework.stereotype.Component;
+
+import java.util.Set;
 
 @Component
 public class ProfanityValidator implements ConstraintValidator<NoProfanity, String> {
@@ -15,7 +18,45 @@ public class ProfanityValidator implements ConstraintValidator<NoProfanity, Stri
     }
 
     @Override
-    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
-        return false;
+    public boolean isValid(String s, ConstraintValidatorContext context) {
+
+        // Make sure string is ASCII chars only
+        if (!StringUtils.isAsciiString(s)) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate("Text contains non-ASCII characters").addConstraintViolation();
+            return false;
+        }
+
+        // normalise text so that any leetspeak is substituted and then remove any other non-aplhabetic chars
+        s = StringUtils.normaliseString(s);
+        s = StringUtils.removeNonAlphabeticChars(s);
+
+        // Load bad word lists
+        Set<String> wordOnlyBad = profanityDictionaryLoader.getWordOnlyBadWords();
+        Set<String> alwaysBad = profanityDictionaryLoader.getAlwaysBadWords();
+
+        // If string is a word that is bad by itself, fail validation
+        for (String wordOnlyBadWord : wordOnlyBad) {
+            if (s.equals(wordOnlyBadWord)) {
+                context.disableDefaultConstraintViolation();
+                context.buildConstraintViolationWithTemplate(
+                                "Text is a prohibited word")
+                        .addConstraintViolation();
+                return false;
+            }
+        }
+
+        // if string contains a word that is bad regardless of context, fail validation
+        for (String alwaysBadWord: alwaysBad) {
+            if (s.contains(alwaysBadWord)) {
+                context.disableDefaultConstraintViolation();
+                context.buildConstraintViolationWithTemplate(
+                                "Text contains prohibited content")
+                        .addConstraintViolation();
+                return false;
+            }
+        }
+
+        return true;
     }
 }
