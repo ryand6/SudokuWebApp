f59fa8dbb49815d8d1a497cd12b9e34e6136c216
R D <ryandowney64@yahoo.com>
2025-09-19 22:32:45 +0100

    Added authentication for all messaging endpoints to ensure only relevant users can send messages to groups they are a member of

--- backend/src/main/java/com/github/ryand6/sudokuweb/config/WebSocketSecurityConfig.java
+++ backend/src/main/java/com/github/ryand6/sudokuweb/config/WebSocketSecurityConfig.java
@@ -4,6 +4,7 @@ import com.github.ryand6.sudokuweb.dto.UserDto;
 import com.github.ryand6.sudokuweb.services.GameService;
 import com.github.ryand6.sudokuweb.services.LobbyService;
 import com.github.ryand6.sudokuweb.services.UserService;
+import com.github.ryand6.sudokuweb.util.OAuthUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.authorization.AuthorizationManager;
@@ -35,14 +36,17 @@ public class WebSocketSecurityConfig {
     @Bean
     public AuthorizationManager<Message<?>> messageAuthorizationManager(MessageMatcherDelegatingAuthorizationManager.Builder messages) {
         messages
-                .simpDestMatchers("/app/**").authenticated()
+                .simpDestMatchers("/app/user/{providerId}")
+                    .access(this::checkUserProviderIdMatch)
+                // Ensure that users can only send messages to lobbies that they are a member of
+                .simpDestMatchers("/app/lobby/{lobbyId}")
+                    .access(this::checkLobbyMembership)
+                // Ensure that users can only send messages to games that they are a member of
+                .simpDestMatchers("/app/game/{gameId}")
+                    .access(this::checkGameMembership)
                 // Ensure specific user endpoint can only be subscribed to by that user
                 .simpSubscribeDestMatchers("/topic/user/{providerId}")
-                    .access(((authentication, context) -> {
-                        String providerId = context.getVariables().get("providerId");
-                        // Uses unique OAuth2 provider ID to authenticate the user subscribing
-                        return new AuthorizationDecision(authentication.get().getName().equals(providerId));
-                    }))
+                    .access(this::checkUserProviderIdMatch)
                 // Ensure that users can only subscribe to lobbies that they are a member of
                 .simpSubscribeDestMatchers("/topic/lobby/{lobbyId}")
                     .access(this::checkLobbyMembership)
@@ -53,23 +57,36 @@ public class WebSocketSecurityConfig {
         return messages.build();
     }
 
-    // Uses Authentication context to find user in DB and confirm if they're an active member in the lobby
-    private AuthorizationDecision checkLobbyMembership(Supplier<Authentication> authenticationSupplier, MessageAuthorizationContext<?> context) {
+    // Ensure endpoint variable matches user's OAuth2 unique ID
+    private AuthorizationDecision checkUserProviderIdMatch(Supplier<Authentication> authenticationSupplier, MessageAuthorizationContext<?> context) {
         Authentication authentication = authenticationSupplier.get();
         OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) authentication;
         OAuth2User principal = token.getPrincipal();
-        UserDto user = userService.getCurrentUserByOAuth(principal, token);
+        String providerId = context.getVariables().get("providerId");
+        String userProviderId = OAuthUtil.retrieveOAuthProviderId(token.getAuthorizedClientRegistrationId(), principal);
+        return new AuthorizationDecision(userProviderId.equals(providerId));
+    }
+
+    // Uses Authentication context to find user in DB and confirm if they're an active member in the lobby
+    private AuthorizationDecision checkLobbyMembership(Supplier<Authentication> authenticationSupplier, MessageAuthorizationContext<?> context) {
+        UserDto user = resolveUserDtoFromAuthenticationSupplier(authenticationSupplier);
         Long lobbyId = Long.valueOf(context.getVariables().get("lobbyId"));
         return new AuthorizationDecision(lobbyService.isUserInLobby(user.getId(), lobbyId));
     }
 
+    // Uses Authentication context to find user in DB and confirm if they're a member of the game
     private AuthorizationDecision checkGameMembership(Supplier<Authentication> authenticationSupplier, MessageAuthorizationContext<?> context) {
+        UserDto user = resolveUserDtoFromAuthenticationSupplier(authenticationSupplier);
+        Long gameId = Long.valueOf(context.getVariables().get("gameId"));
+        return new AuthorizationDecision(gameService.isUserInGame(user.getId(), gameId));
+    }
+
+    // Get UserDto using Authentication object
+    private UserDto resolveUserDtoFromAuthenticationSupplier(Supplier<Authentication> authenticationSupplier) {
         Authentication authentication = authenticationSupplier.get();
         OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) authentication;
         OAuth2User principal = token.getPrincipal();
-        UserDto user = userService.getCurrentUserByOAuth(principal, token);
-        Long gameId = Long.valueOf(context.getVariables().get("gameId"));
-        return new AuthorizationDecision(gameService.isUserInGame(user.getId(), gameId));
+        return userService.getCurrentUserByOAuth(principal, token);
     }
 
 }
