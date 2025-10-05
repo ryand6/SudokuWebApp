0dce96fceefd6a89aa577280c9b574e5a5a54396
R D <ryandowney64@yahoo.com>
2025-10-05 19:12:51 +0100

    Implemented modal open/close functionality

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Button } from "./ui/button";
 
 interface ModalProps {
     isOpen: boolean,
@@ -10,14 +11,15 @@ interface ModalProps {
 export function Modal({ isOpen, onClose, children }: ModalProps) {
     if (!isOpen) return null;
     return ReactDOM.createPortal(
-        <div className="fixed top-[10%] left-[10%] w-[80%] h-[80%] bg-white z-1000 flex">
+        <div className="fixed top-[10%] left-[10%] w-[80%] h-[80%] bg-white z-1000 flex border-[2px] border-border rounded-md">
             <div className="flex w-[100%] h-[100%] relative">
-                <button 
-                    className="absolute top-2 right-2 text-gray-500 hover:text-black"
+                <Button 
+                    className="absolute cursor-pointer top-2 right-2 text-dark-gray text-[25px] hover:font-bold"
                     onClick={onClose}
+                    variant="secondary"
                 >
                     &times;
-                </button>
+                </Button>
                 {children}
             </div>
         </div>,
--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -3,6 +3,8 @@ import { useCurrentUser } from "../hooks/users/useCurrentUser";
 import { useUserRank } from "../hooks/users/useUserRank";
 import { useTopFivePlayers } from "../hooks/users/useTopFivePlayers";
 import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import { Modal } from "@/components/Modal";
 
 export function DashboardPage() {
 
@@ -20,19 +22,20 @@ export function DashboardPage() {
                 </div>
             </header>
             <div id="dashboard-content" className="flex flex-1">
-                <div id="actions-pane" className="p-[15px] m-[15px] bg-[#fff] border-[8px] border-blue-200 rounded-md h-1/3 flex flex-col w-1/4 shadow-[0_2px_5px_rgba(0,0,0,0.1)]">
+                <div id="actions-pane" className="p-[15px] m-[15px] bg-card border-[2px] rounded-md h-1/3 flex flex-col w-1/4 shadow-[0_2px_5px_rgba(0,0,0,0.1)]">
                     <h2>Lobby Actions</h2>
-                    <Link to="/lobby/create-lobby">Create New Lobby</Link>
-                    <button>Join Lobby</button>
+                    <Button variant="outline" className="m-1"><Link to="/lobby/create-lobby">Create New Lobby</Link></Button>
+                    <Button variant="outline" className="m-1 cursor-pointer" onClick={() => setModalOpen(true)}>Join Lobby</Button>
                 </div>
-                <div id="mini-leaderboard" className="p-[15px] m-[15px] bg-[#fff] border-[8px] border-blue-200 rounded-md h-1/3 flex flex-col w-1/4 shadow-[0_2px_5px_rgba(0,0,0,0.1)]">
+                <div id="mini-leaderboard" className="p-[15px] m-[15px] bg-card border-[2px] rounded-md h-1/3 flex flex-col w-1/4 shadow-[0_2px_5px_rgba(0,0,0,0.1)]">
                     <h3>Top 5 Players</h3>
                     <ol>
                         {topFivePlayersDto?.topFivePlayers.map((player, index) => 
-                            <li><span>{player.username}</span> | Score: <span>{player.score.totalScore}</span> | Rank: #<span>{index + 1}</span></li>
+                            <li key={index}><span>{player.username}</span> | Score: <span>{player.score.totalScore}</span> | Rank: #<span>{index + 1}</span></li>
                         )}
                     </ol>
                 </div>
+                <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)}><></></Modal>
 
             </div>
         </div>
